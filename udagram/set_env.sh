7bf13bc77a73626319082a840a912752500032cd
farahmahmoud70 <farah.mahmoud7080@gmail.com>
2023-03-03 13:49:19 +0200

    running udagram - eb deploy

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=udagram_db
+export POSTGRES_PASSWORD=udagram_db1234
+export POSTGRES_HOST=udacity-udagram-ci-cd-db.cr4iq0mdn98q.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::udacity-udagram-ci-cd
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
--- udagram/udagram-api/src/aws.ts
+++ udagram/udagram-api/src/aws.ts
@@ -1,9 +1,14 @@
-import AWS = require("aws-sdk");
+// import AWS_S3 = require("@aws-sdk/client-s3");
+
+// const { S3 } = AWS_S3;
+
+import { S3 } from "aws-sdk";
+
 import { config } from "./config/config";
 
 //Credentials are auto set according to the documentation https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials-node.html and the default profile is "Default anyway"
 
-export const s3 = new AWS.S3({
+export const s3 = new S3({
   signatureVersion: "v4",
   region: config.aws_region,
   params: { Bucket: config.aws_media_bucket },
